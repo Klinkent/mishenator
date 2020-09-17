b4f763fae8952de9e58d477413be6e09792d3fdd
Макаренко Алексей Алексеевич <Makarenko.A.Alek@sberbank.ru>
2020-09-17 11:22:58 +0300

    Кристофари

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,10 @@ module.exports = {
         include: path.resolve(__dirname, "src/html/includes"),
         use: ["raw-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "url-loader?limit=100000",
+      },
     ],
   },
   plugins: [
